5b927a6c0b0ef0410a57daa64d8a0f9072214309
lattepapa <whitelatte46@gmail.com>
2022-04-21 18:27:17 +0900

    modify templates for adopting input text & save db

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from accountapp.models import HellWorld
+
+
 def hell_world(request):
     # return HttpResponse('Hell World')
     if request.method == "POST":
-        return render(request, 'accountapp/hell_world.html', context={'isAllowed': 'POST METHOD!!!'})
-    else :
+        # 1. 텍스트 input 창에 입력된 값을 temp 변수에 저장
+        temp = request.POST.get('hell_world_input')
+
+        # 2. 데이터베이스에 temp 변수 값 저장
+        new_hell_world = HellWorld()
+        new_hell_world.testText = temp
+        new_hell_world.save()
+
+        return render(request, 'accountapp/hell_world.html', context={'isClicked': new_hell_world})
+    else:
         return render(request, 'accountapp/hell_world.html')

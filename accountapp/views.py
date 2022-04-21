6f320a647227903dbca8935dbdda7ca8651631cd
lattepapa <whitelatte46@gmail.com>
2022-04-21 19:47:49 +0900

    add HttpResponseRedirect, Routing Reversing, and list forloop calling

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 from accountapp.models import HellWorld
 
@@ -15,6 +16,9 @@ def hell_world(request):
         new_hell_world.testText = temp
         new_hell_world.save()
 
-        return render(request, 'accountapp/hell_world.html', context={'isClicked': new_hell_world})
+        #4. 새로고침 시 반복적으로 POST 기능이 수행되지 않도록, accountapp:hell_world 로의 라우팅에 'redirect' 기능을 사용
+        return HttpResponseRedirect(reverse('accountapp:hell_world'))
     else:
-        return render(request, 'accountapp/hell_world.html')
+        # 3. 데이터베이스의 HellWorld 객체의 모든 리스트를 추출
+        hell_world_list = HellWorld.objects.all()
+        return render(request, 'accountapp/hell_world.html', context={'showAll': hell_world_list})
